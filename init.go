07a89b34c9fa7c17790a0e6b4d686c1ec40ee362
Gaukas Wang <i@gaukas.wang>
2021-11-17 19:41:25 -0700

    deepsource.io suggested fix

--- init.go
+++ init.go
@@ -3,7 +3,5 @@ package utrojan
 import "github.com/TunnelWork/Ulysses.Lib/server"
 
 func init() {
-	server.RegisterServer("utrojan", func( /*db *sql.DB, */ instanceID string, serverConfiguration interface{}) (server.ProvisioningServer, error) {
-		return NewProvisioningServer(instanceID, serverConfiguration)
-	})
+	server.RegisterServer("utrojan", NewProvisioningServer)
 }
